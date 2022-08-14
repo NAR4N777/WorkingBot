0292a498dd0eceb5dc9fc996ec0e6d0841882610
NAR4N777 <88475641+NAR4N777@users.noreply.github.com>
2022-08-14 14:35:44 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,51 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let duit = `*────── 「 DONATE 」 ──────*
+
+Donate kak, Buat beli jajan
+
+Contact person Owner:
+wa.me/6285713110263`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/c987342b2a9c2e408b6dc.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '🏧Trakteer',
+               url: 'https://trakteer.id/fann-hack-iigap'
+             }
+
+           },
+               {
+             urlButton: {
+               displayText: '🏧Dana',
+               phoneNumber: 'https://link.dana.id/qr/3sqn7c2y'
+             }
+           },
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)|bagiduit$/i
+
+module.exports = handler
---
+++ plugins/inf-sc.js
@@ -0,0 +1,12 @@
+let handler = async m => m.reply(`
+            Bot Gratis kok.
+Tinggal chat owner 👇
+wa.me/6285713110263
+`.trim()) //
+
+handler.customPrefix = /^(.sc|.script|sc|script)$/i
+handler.command = new RegExp
+
+handler.limit = false
+handler.mods = false 
+module.exports = handler
